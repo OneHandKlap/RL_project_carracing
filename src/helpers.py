fe0c8d66442ff1660f1b54d6f236608889eab778
Chris Wang <christian.gnaw@gmail.com>
2021-03-07 14:13:49 -0500

    Added WIP tf_agents implementation, video output is working and random-policy agent works

---
+++ src/helpers.py
@@ -0,0 +1,27 @@
+import IPython
+import imageio
+import base64
+
+def embed_mp4(filename):
+  """Embeds an mp4 file in the notebook."""
+  video = open(filename,'rb').read()
+  b64 = base64.b64encode(video)
+  tag = '''
+  <video width="640" height="480" controls>
+    <source src="data:video/mp4;base64,{0}" type="video/mp4">
+  Your browser does not support the video tag.
+  </video>'''.format(b64.decode())
+
+  return IPython.display.HTML(tag)
+
+def create_policy_eval_video(eval_env, eval_py_env, policy, filename, num_episodes=1, fps=30):
+  filename = filename + ".mp4"
+  with imageio.get_writer(filename, fps=fps) as video:
+    for _ in range(num_episodes):
+      time_step = eval_env.reset()
+      video.append_data(eval_py_env.render())
+      while not time_step.is_last():
+        action_step = policy.action(time_step)
+        time_step = eval_env.step(action_step.action)
+        video.append_data(eval_py_env.render())
+  return embed_mp4(filename)
