43c5f57600d7f879800a7ee4466ac18a07e2ed8f
Christian Wang <christian.gnaw@gmail.com>
2021-03-16 12:13:43 -0400

    lol

--- src/helpers.py
+++ src/helpers.py
@@ -2,6 +2,42 @@ import IPython
 import imageio
 import base64
 
+from collections import namedtuple
+import random
+
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        self.position = 0
+
+    def push(self, *args):
+        """Saves a transition."""
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.position] = Transition(*args)
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+def get_screen():
+    screen = env.render(mode='rgb_array').transpose((2, 0, 1))
+    screen = np.ascontiguousarray(screen, dtype=np.float32) / 255
+    screen = torch.from_numpy(screen)
+
+
+
+
+
 def embed_mp4(filename):
   """Embeds an mp4 file in the notebook."""
   video = open(filename,'rb').read()
