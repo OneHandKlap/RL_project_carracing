1ff103e759045a7bae0193a8c9d9a397dff850ef
Chris Wang <christian.gnaw@gmail.com>
2021-03-18 15:12:55 -0400

    added virtualdisplay fix for gcp

--- src/rl_train.py
+++ src/rl_train.py
@@ -15,6 +15,16 @@ import torch.optim as optim
 import imageio
 import base64
 
+from pyvirtualdisplay import Display
+display = Display(visible=0, size=(1400, 900))
+display.start()
+
+is_ipython = 'inline' in plt.get_backend()
+if is_ipython:
+    from IPython import display
+
+plt.ion()
+
 
 resize = T.Compose([T.ToPILImage(),
                     T.Resize(40, interpolation=Image.CUBIC),
