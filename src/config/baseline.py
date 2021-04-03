d78ecbd6a2ab3c8617c1decce06cd1e4bae34352
Chris Wang <christian.gnaw@gmail.com>
2021-04-03 02:27:52 -0400

    Updated action space

--- src/config/baseline.py
+++ src/config/baseline.py
@@ -10,5 +10,10 @@ config = {
     "EPSILON_MIN": 0.1,  # min epsilon
     "EPSILON_DECAY": 0.9999,  # rate at which epsilon decays
     "TARGET_UPDATE_INTERVAL": 10,  # interval at which to update target Q,
-    "ACTION_SPACE": [[1, 0, 0], [-1, 0, 0], [0, 1, 0], [0, 0, 1]]  # action space [direction, throttle, brake]
+    "ACTION_SPACE": list({"turn_left": [-1, 0, 0], "turn_right": [1, 0, 0], "go": [0, 1, 0],
+                          "go_left": [-1, 1, 0], "go_right": [1, 1, 0], "brake": [0, 0, 1],
+                          "brake_left": [-1, 0, 1], "brake_right": [1, 0, 1], "slight_turn_left": [-.3, 0, 0],
+                          "slight_turn_right": [.3, 0, 0], "slight_go": [0, .3, 0], "slight_go_left": [-.3, .3, 0],
+                          "slight_go_right": [.3, .3, 0], "slight_brake": [0, 0, .3], "slight_brake_left": [-.3, 0, .3],
+                          "slight_brake_right": [.3, 0, .3]}.values())  # action space [direction, throttle, brake]
 }
