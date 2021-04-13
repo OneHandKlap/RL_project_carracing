29edab53c03a5a2c8dcc88dc60e5f2c930387092
Chris Wang <christian.gnaw@gmail.com>
2021-04-13 13:33:08 -0400

    Added onyx models

--- src/models/dqn_basic.py
+++ src/models/dqn_basic.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -12,6 +13,7 @@ class DQN_Basic(nn.Module):
         self.bn2 = nn.BatchNorm2d(32)
         self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
         self.bn3 = nn.BatchNorm2d(32)
+        self.flatten = nn.Flatten()
 
         # Number of Linear input connections depends on output of conv2d layers
         # and therefore the input image size, so compute it.
@@ -26,10 +28,13 @@ class DQN_Basic(nn.Module):
     # during optimization. Returns tensor([[left0exp,right0exp]...]).
     def forward(self, x):
         x = F.relu(self.bn1(self.conv1(x)))
-        #print(x.shape)
         x = F.relu(self.bn2(self.conv2(x)))
-        #print(x.shape)
         x = F.relu(self.bn3(self.conv3(x)))
-        #print(x.shape)
-        
-        return self.head(x.view(x.size(0), -1))
+        x = self.flatten(x)
+        return self.head(x)
+
+
+x = torch.randn(128, 9, 40, 60)
+model = DQN_Basic(60, 40, 3, 16)
+model(x)
+torch.onnx.export(model, x, "dqn_basic.onnx", input_names=['input'], output_names=['output'], do_constant_folding=True)
--- src/models/dqn_deep.py
+++ src/models/dqn_deep.py
@@ -11,10 +11,8 @@ class DQN_Deep(nn.Module):
         self.bn1 = nn.BatchNorm2d(16)
         self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
         self.bn2 = nn.BatchNorm2d(32)
-        self.max_pool1=nn.MaxPool2d(3)
-        # self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
-        # self.bn3 = nn.BatchNorm2d(32)
-        #self.max_pool2=nn.MaxPool2d()
+        self.max_pool1 = nn.MaxPool2d(3)
+        self.flatten = nn.Flatten()
 
         # Number of Linear input connections depends on output of conv2d layers
         # and therefore the input image size, so compute it.
@@ -23,26 +21,22 @@ class DQN_Deep(nn.Module):
         convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w)))
         convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h)))
         linear_input_size = convw * convh * 32
-
-        self.dense1 = nn.Linear(256, 32)
-        self.head = nn.Linear(32,outputs)
-
+        self.dense1 = nn.Linear(linear_input_size, 32)
+        self.head = nn.Linear(32, outputs)
 
     # Called with either one element to determine next action, or a batch
     # during optimization. Returns tensor([[left0exp,right0exp]...]).
+
     def forward(self, x):
         x = F.relu(self.bn1(self.conv1(x)))
-
         x = F.relu(self.bn2(self.conv2(x)))
-
         x = self.max_pool1(x)
-
-        x= x.view(x.size(0), -1)
-
-        #x = F.relu(self.bn3(self.conv3(x)))
-        #print(x.shape)
-        
+        x = self.flatten(x)
         x = self.dense1(x)
+        return self.head(x)
 
 
-        return self.head(x.view(x.size(0), -1))
+x = torch.randn(128, 9, 40, 60)
+model = DQN_Deep(60, 40, 3, 16)
+model(x)
+torch.onnx.export(model, x, "dqn_deep.onnx", input_names=['input'], output_names=['output'], do_constant_folding=True)
